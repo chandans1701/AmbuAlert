51fdcb5e75a90da6f10b9737b505cef923a5641e
chandans1701 <chandanhs174@gmail.com>
2026-05-22 14:52:05 +0530

    chore: support environment variables for deployment (dynamic port & backend URL)

--- backend/server.js
+++ backend/server.js
@@ -65,7 +65,7 @@ setInterval(() => {
   });
 }, 33);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Backend Server running on port ${PORT}`);
 });
